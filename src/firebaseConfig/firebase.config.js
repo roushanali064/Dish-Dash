c1321d02bd0c2a4a82deb935a4a9ce31a110c220
roushanali064 <roushanrhaman064@gmail.com>
2023-05-02 22:12:01 +0600

    all initial setup done

---
+++ src/firebaseConfig/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyATBC-_dLQ14RkBcj2WyY5gTSr75dFRch4",
+  authDomain: "dishdash-f12cb.firebaseapp.com",
+  projectId: "dishdash-f12cb",
+  storageBucket: "dishdash-f12cb.appspot.com",
+  messagingSenderId: "270517763106",
+  appId: "1:270517763106:web:e65c5737b4460a70f19e7b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
